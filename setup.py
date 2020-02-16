9fdcd8847c2993de65eebad22401df136d85776e
Leonardo Ferreira <ppxlf2@hppcaptgpu.physics.nottingham.ac.uk>
2020-02-16 23:00:44 +0000

    add setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='prober',
+      version='0.01',
+      description='Push messages to telegram',
+      url='http://github.com/astroferreira/prober',
+      author='Leonardo de Albernaz Ferreira',
+      author_email='leonardo.ferreira@nottingham.ac.uk',
+      license='MIT',
+      moduless=['prober'],
+      zip_safe=False)
